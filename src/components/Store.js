8ba1889b3648ed882a5505186c6a6f21e753902e
kesavindia <kesavindia83@gmail.com>
2023-06-28 11:16:44 +0530

    d

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-function Store({ setIsLoggedIn }) {
-  return (
-    <div className="store">
-      <h2>Welcome to the store!</h2>
-      <button onClick={() => setIsLoggedIn(false)} className="logout-btn">
-        Logout
-      </button>
-    </div>
-  );
+function Store(props) {
+    const {setIsLoggedIn} = props
+    return (
+        <div className='store'>
+            <h2>Welcome to the store!</h2>
+            <button onClick={()=>setIsLoggedIn(false)} className="logout-btn">Logout</button>
+        </div>
+    );
 }
 
 export default Store;
