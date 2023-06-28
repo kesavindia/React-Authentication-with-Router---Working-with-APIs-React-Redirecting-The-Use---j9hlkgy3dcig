e9533b9e0ff1bdc00e10222860db0e6dfd78e12c
kesavindia <kesavindia83@gmail.com>
2023-06-28 11:07:08 +0530

    done

--- src/components/Store.js
+++ src/components/Store.js
@@ -5,7 +5,7 @@ function Store(props) {
     return (
         <div className='store'>
             <h2>Welcome to the store!</h2>
-            <button onClick={()=>setIsLoggedIn(false)}className="logout-btn">Logout</button>
+            <button onClick={()=>setIsLoggedIn(false)} className="logout-btn">Logout</button>
         </div>
     );
 }
